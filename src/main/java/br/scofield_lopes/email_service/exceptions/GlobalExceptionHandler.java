5f5d0e71d4dcf07326b0d35aa55f39ef832abefa
JanseyScofield <janscofield10@gmail.com>
2026-01-18 21:30:36 -0300

    Add GlobalExceptionHandle

---
+++ src/main/java/br/scofield_lopes/email_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.scofield_lopes.email_service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.scofield_lopes.email_service.dtos.ResponseSendEmailDto;
+import br.scofield_lopes.email_service.enums.EmailStatus; 	
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(EmailException.class)
+		public ResponseEntity<ResponseSendEmailDto> handleEmailBadRequest(EmailException ex){
+		ResponseSendEmailDto response = new ResponseSendEmailDto(EmailStatus.ERROR, ex.getMessage());
+		return new ResponseEntity<ResponseSendEmailDto>(response, HttpStatus.BAD_REQUEST);
+	}
+}
